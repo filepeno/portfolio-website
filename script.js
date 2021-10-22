be707aee38d9b59946365eee22a19331982f0a2a
Filip Soudakov <fili4252@stud.kea.dk>
2021-10-22 11:48:47 +0200

    loaded svg

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 "use strict";
+
+window.addEventListener("DOMContentLoaded", loadSvg);
+
+async function loadSvg() {
+  console.log("DOM loaded");
+  const heroSvg = await fetch("assets/img/portfolio_home-page_graphic-03-01.svg");
+  const heroSvgText = await heroSvg.text();
+  document.querySelector("section#hero").innerHTML = heroSvgText;
+}
