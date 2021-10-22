3b7d7c26dca25b8a1034790a38b22b88fec301c7
Filip Soudakov <fili4252@stud.kea.dk>
2021-10-22 16:43:08 +0200

    added fade-in animation on each shape, and tried fade-out animation (not working)

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ async function loadSvg() {
   document.querySelector("section#hero").innerHTML = heroSvgText;
   addShapesToHtmlObject();
   makeShapesTransparent();
-  trackHoverOnShapes();
+  trackMouseOverOnShapes();
 }
 
 function addShapesToHtmlObject() {
@@ -28,10 +28,11 @@ function makeShapesTransparent() {
   HTML.shapes.forEach((shape) => {
     shape.style.fill = "transparent";
     shape.style.stroke = "transparent";
+    shape.style.opacity = "";
   });
 }
 
-function trackHoverOnShapes() {
+function trackMouseOverOnShapes() {
   //add eventlistener to groups
   HTML.shapes.forEach((shape) => {
     shape.addEventListener("mouseover", getShape);
@@ -40,9 +41,51 @@ function trackHoverOnShapes() {
 
 function getShape() {
   console.log(this);
-  changeFill(this);
+  addFill(this);
+  //   trackMouseOutOnShapes(this)
 }
 
-function changeFill(shape) {
+function addFill(shape) {
   shape.style = "";
+  animateFadeInOnShape(shape);
+  //***take random colour from the fills and colour the shape***
+  //   shape.style.fill = fills[Math.floor(Math.random() * 10)];
+}
+
+function animateFadeInOnShape(shape) {
+  const fadeIn_props = {
+    duration: 3000,
+    iterations: 1,
+    fill: "forwards",
+    easing: "ease-in-out",
+    composite: "add",
+  };
+  const fadeIn_kf = [{ opacity: 0 }, { opacity: 1 }];
+  const fadeAnimation = shape.animate(fadeIn_kf, fadeIn_props);
+  trackMouseOutFromShape(shape, fadeAnimation);
+}
+
+function trackMouseOutFromShape(shape, fadeAnimation) {
+  shape.addEventListener("mouseout", animateFadeOutOnShape);
+  function animateFadeOutOnShape() {
+    console.log("mouse left the shape");
+    fadeAnimation.pause();
+    const fadeCurrentTime = fadeAnimation.currentTime;
+    console.log(fadeCurrentTime);
+    let currentOpacity;
+    if (fadeCurrentTime > 10) {
+      currentOpacity = fadeCurrentTime / 3000;
+      console.log(currentOpacity);
+      const fadeOut_props = {
+        duration: fadeCurrentTime,
+        iterations: 1,
+        fill: "forwards",
+        easing: "ease-in-out",
+        composite: "add",
+      };
+      const fadeOut_kf = [{ opacity: currentOpacity }, { opacity: 0 }];
+      fadeAnimation.cancel();
+      setTimeout(shape.animate(fadeOut_kf, fadeOut_props), 1000);
+    }
+  }
 }
