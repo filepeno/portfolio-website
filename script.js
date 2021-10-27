68bc417de018a495a2f7d70f6d9cba0b020c5721
Filip Soudakov <fili4252@stud.kea.dk>
2021-10-27 18:03:33 +0200

    adjusted svg size and position

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ async function loadSvg() {
   console.log("DOM loaded");
   const heroSvg = await fetch("assets/img/portfolio_home-page_graphic-03-01.svg");
   const heroSvgText = await heroSvg.text();
-  document.querySelector("section#hero").innerHTML = heroSvgText;
+  document.querySelector("section#hero-svg").innerHTML = heroSvgText;
   startDelegator();
 }
 
