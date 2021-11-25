04481e5ecbc96441977e65793e04f3b0a02e5b72
Filip Soudakov <fili4252@stud.kea.dk>
2021-11-25 09:03:16 +0100

    added opacity transition

--- js/components/loading.js
+++ js/components/loading.js
@@ -11,4 +11,5 @@ export function onReady(callback) {
 
 export function setVisible(selector, visible) {
   document.querySelector(selector).style.visibility = visible ? "visible" : "hidden";
+  document.querySelector(selector).style.opacity = visible ? "100%" : "0";
 }
